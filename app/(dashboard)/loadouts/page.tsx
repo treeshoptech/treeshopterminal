04d3b01bb013681d84e7ad80d4264efc5dcae9e3
Jeremiah Anderson <treeshoptech@icloud.com>
2025-10-10 06:33:38 -0400

    Add missing pages - no more 404s

---
+++ app/(dashboard)/loadouts/page.tsx
@@ -0,0 +1,7 @@
+export default function LoadoutsPage() {
+  return (
+    <div>
+      <h1>Loadouts</h1>
+    </div>
+  );
+}
---
+++ app/(dashboard)/work-orders/page.tsx
@@ -0,0 +1,7 @@
+export default function WorkOrdersPage() {
+  return (
+    <div>
+      <h1>Work Orders</h1>
+    </div>
+  );
+}
